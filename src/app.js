4015b243c8b53706e2c188f159ebdd4321dcb544
Digory <digory1@googlemail.com>
2018-09-10 18:00:33 +0100

    Added prime_checker bindings

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const NumberInputView = require('./views/number_input_view.js');
+const PrimeChecker = require('./models/prime_checker.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   const numberInputView = new NumberInputView();
   numberInputView.bindEvents();
-
-  
+  const primeChecker = new PrimeChecker();
+  primeChecker.bindEvents();
 });
